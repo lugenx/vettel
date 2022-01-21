ec668a0e7895308429a06d519e219ed47f1c6297
lugenx <elgunasgerov@gmail.com>
2022-01-20 21:31:12 -0500

    fixes #1

--- commands/standings.mjs
+++ commands/standings.mjs
@@ -3,51 +3,51 @@ export const name = "standings";
 export const description =
   "This command will give the standings of a specific year";
 export function execute(message, args) {
-  let command = message.content.split(" ")[0];
-  let currentYear = new Date().getFullYear();
-  let askedYear = args[0] === undefined ? "current" : args[0];
+  const currentYear = new Date().getFullYear();
 
-  if ((askedYear.length !== 4 || isNaN(askedYear)) && askedYear !== "current") {
+  //User inputs: +command arg
+  const command = message.content.split(" ")[0];
+  const arg = args[0] || "current";
+  const argIsAYear = arg && arg.length === 4 && !isNaN(arg) && arg < 3000;
+
+  if (!argIsAYear && arg !== "current") {
     return message.channel.send(
       `Enter a \`YEAR\` after the \`${command}\`,\n*For example:* \`${command} 1988\`. Learn more by using \`+help\` command.`
-      //, \nYou can use \`current\` for the year to check current standings quickly.\n*For example:*\`${command} current\`
     );
-  } else if (
-    askedYear > currentYear &&
-    !isNaN(askedYear) &&
-    askedYear.length === 4
-  ) {
-    return message.channel.send(
-      `I don't know what is going to happen in ${askedYear}`
-    );
-  } else {
-    async function result() {
-      try {
-        const response = await fetch(
-          `https://ergast.com/api/f1/${askedYear}/driverStandings.json`
-        );
-
-        const data = await response.json();
-        const table = await data.MRData.StandingsTable;
-        const standingList = await table.StandingsLists[0];
-        const driverStandings = await standingList.DriverStandings;
-        const season = standingList.season;
-
-        let standings = driverStandings.map(
-          (p) =>
-            `${p.position}  ${p.Driver.givenName[0]}. ${p.Driver.familyName}  -  ${p.points} points`
-        );
-
-        message.channel.send(
-          `Here is the driver standings of **${season}** season. \n ${standings.join(
-            " \n"
-          )}`
-        );
-      } catch (err) {
-        console.log(err);
-        message.channel.send("hmm... :thinking:");
-      }
-    }
-    return result();
   }
+
+  if (argIsAYear && arg > currentYear) {
+    return message.channel.send(
+      `I don't know what is going to happen in ${arg}`
+    );
+  }
+
+  async function result() {
+    try {
+      const response = await fetch(
+        `https://ergast.com/api/f1/${arg}/driverStandings.json`
+      );
+
+      const data = await response.json();
+      const table = await data.MRData.StandingsTable;
+      const standingList = await table.StandingsLists[0];
+      const driverStandings = await standingList.DriverStandings;
+      const season = standingList.season;
+
+      let standings = driverStandings.map(
+        (p) =>
+          `${p.position}  ${p.Driver.givenName[0]}. ${p.Driver.familyName}  -  ${p.points} points`
+      );
+
+      let standingsStr = standings.join("\n");
+
+      message.channel.send(
+        `Here is the driver standings of **${season}** season. \n ${standingsStr}`
+      );
+    } catch (err) {
+      console.log(err);
+      message.channel.send("hmm... :thinking:");
+    }
+  }
+  return result();
 }
